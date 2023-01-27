172cf77f54e568a6ecd07e3e5ce37e7453aa71e8
gralucky254 <hehomichael275@gmail.com>
2023-01-27 10:09:01 -0800

    main.h

--- main.h
+++ main.h
@@ -43,7 +43,7 @@ int _printf(const char *format, ...);
 int handle_print(const char *fmt, int *i,
 va_list list, char buffer[], int flags, int width, int precision, int size);
 
-/****** FUNCTIONS ******/
+/****************** FUNCTIONS ******************/
 
 /* Funtions to print chars and strings */
 int print_char(va_list types, char buffer[],
@@ -84,9 +84,11 @@ int get_width(const char *format, int *i, va_list list);
 int get_precision(const char *format, int *i, va_list list);
 int get_size(const char *format, int *i);
 
+/*Function to print string in reverse*/
 int print_reverse(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
 
+/*Function to print a string in rot 13*/
 int print_rot13string(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
 
@@ -104,7 +106,7 @@ int write_unsgnd(int is_negative, int ind,
 char buffer[],
 	int flags, int width, int precision, int size);
 
-/****** UTILS ******/
+/****************** UTILS ******************/
 int is_printable(char);
 int append_hexa_code(char, char[], int);
 int is_digit(char);
